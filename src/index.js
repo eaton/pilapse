5d7382f33d8cb4ed8ab849e27e646c654cf714ea
Jeff Eaton <jeff@eaton.fyi>
2022-06-29 18:58:07 -0500

    Simple stubbed routes

--- src/index.js
+++ src/index.js
@@ -18,6 +18,40 @@ const init = async () => {
             return '<h1>hello world!</h1>'
         }
     });
+
+    server.route({
+        method: 'GET',
+        path: '/preview',
+        handler: (request, h) => {
+            return '<h1>Preview goes here!</h1>'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/snapshot',
+        handler: (request, h) => {
+            return '<h1>Trigger a single snapshot, return the image</h1>'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/timelapse',
+        handler: (request, h) => {
+            return '<h1>Start a time lapse recording</h1>'
+        }
+    });
+
+    server.route({
+        method: 'GET',
+        path: '/slowmotion',
+        handler: (request, h) => {
+            return '<h1>Start a slow motion recording</h1>'
+        }
+    });
+
+
     await server.start();
     console.log('Server running on %s', server.info.uri);
 };
